38d43dc6baf0360ee47b42988322c3e26dcaefaf
ariellugo92koder <ariellugo92koderlite@gmail.com>
2017-04-04 16:01:10 -0600

    cambiando el plugin del hover

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,16 +8,52 @@
     {{ Html::style(asset('css/bootstrap/material-kit.css')) }}
     {{ Html::style(asset('css/material_icon/icons_md_icon.css')) }}
     {{ Html::style(asset('css/index.css'))}}
+    {{-- css necesarios para el efecto hover de los servicios --}}
+    {{ Html::style(asset('css/plugins/hoverEfect/common.css')) }}
+    {{ Html::style(asset('css/plugins/hoverEfect/style5.css')) }}
     <title>KoderLite</title>
 </head>
 <body>
     {{-- include de las secciones --}}
     @include('secciones.inicio')
 
+    {{-- include seccion servicios --}}
+    @include('secciones.servicios')
+
     {{ Html::script(asset('js/plugins/jquery.min.js')) }}
     {{ Html::script(asset('js/plugins/tether.js')) }}
     {{ Html::script(asset('js/bootstrap/bootstrap.min.js')) }}
     {{ Html::script(asset('js/bootstrap/material.min.js')) }}
     {{ Html::script(asset('js/bootstrap/material-kit.js')) }}
+    {{-- js necesarios para el efecto hover de los servicios --}}
+    {{ Html::script(asset('js/plugins/hoverEfect/modernizr.custom.79639.js')) }}
+    <script>
+        (function() {
+	
+				function init() {
+					var speed = 330,
+						easing = mina.backout;
+
+					[].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+						var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+							pathConfig = {
+								from : path.attr( 'd' ),
+								to : el.getAttribute( 'data-path-hover' )
+							};
+
+						el.addEventListener( 'mouseenter', function() {
+							path.animate( { 'path' : pathConfig.to }, speed, easing );
+						} );
+
+						el.addEventListener( 'mouseleave', function() {
+							path.animate( { 'path' : pathConfig.from }, speed, easing );
+						} );
+					} );
+				}
+
+				init();
+
+			})();
+    </script>
 </body>
 </html>
---
+++ resources/views/secciones/servicios.blade.php
@@ -0,0 +1,62 @@
+<div class="">
+
+    <section class="main">
+			
+				<ul class="ch-grid">
+					<li>
+						<div class="ch-item ch-img-1">				
+							<div class="ch-info-wrap">
+								<div class="ch-info">
+									<div class="ch-info-front ch-img-1"></div>
+									<div class="ch-info-back">
+										<h3>taxi</h3>
+										<p>by Ana Villa-Zamora <a href="http://drbl.in/ewng">View on Dribbble</a></p>
+									</div>	
+								</div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-2">
+							<div class="ch-info-wrap">
+								<div class="ch-info">
+									<div class="ch-info-front ch-img-2"></div>
+									<div class="ch-info-back">
+										<h3>Stay Foxy</h3>
+										<p>by Arnel Baluyot <a href="http://drbl.in/eQDg">View on Dribbble</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-3">
+							<div class="ch-info-wrap">
+								<div class="ch-info">
+									<div class="ch-info-front ch-img-3"></div>
+									<div class="ch-info-back">
+										<h3>Klava Jinx</h3>
+										<p>by Jamal Charanek <a href="http://drbl.in/ejLW">View on Dribbble</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+                    <li>
+						<div class="ch-item ch-img-3">
+							<div class="ch-info-wrap">
+								<div class="ch-info">
+									<div class="ch-info-front ch-img-3"></div>
+									<div class="ch-info-back">
+										<h3>Klava Jinx</h3>
+										<p>by Jamal Charanek <a href="http://drbl.in/ejLW">View on Dribbble</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+				
+			</section>
+
+</div>
