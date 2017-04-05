2ac2d32891e335c55cb8e7f32b200f6105e68eee
eocongarcia <eocongarcia@gmail.com>
2017-04-05 10:29:45 -0600

    modificando permisos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,25 +10,30 @@
     {{ Html::style(asset('css/index.css'))}}
     {{ Html::style(asset('css/plugins/slitslider/slitslider.css')) }}
     {{ Html::style(asset('css/plugins/slitslider/slider.css')) }}
-
-
     <noscript>
       {{  Html::style(asset('css/plugins/sliteslider/slitesliderNojs.css')) }}
     </noscript>
+
+
     <title>KoderLite</title>
 </head>
 <body>
     {{-- include de las secciones --}}
     @include('secciones.inicio')
 
-    {{ Html::script(asset('js/plugins/slitslider/modernizr.custom.79639.js')) }}
+
+
+
     {{ Html::script(asset('js/plugins/jquery.min.js')) }}
     {{ Html::script(asset('js/plugins/tether.js')) }}
     {{ Html::script(asset('js/bootstrap/bootstrap.min.js')) }}
     {{ Html::script(asset('js/bootstrap/material.min.js')) }}
     {{ Html::script(asset('js/bootstrap/material-kit.js')) }}
-    {{ Html::script(asset('js/index.js'))}}
+    {{ Html::script(asset('js/plugins/slitslider/modernizr.custom.79639.js')) }}
     {{ Html::script(asset('js/plugins/slitslider/jquery.ba-cond.min.js')) }}
     {{ Html::script(asset('js/plugins/slitslider/jquery.slitslider.js'))}}
+    {{ Html::script(asset('js/index.js'))}}
+
+
 </body>
 </html>
