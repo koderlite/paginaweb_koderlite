a9b2320310377b024b398329a798ca5eaed3e2ac
eocongarcia <eocongarcia@gmail.com>
2017-04-05 01:24:01 -0600

    Configuracion inicial close #1

--- resources/views/secciones/inicio.blade.php
+++ resources/views/secciones/inicio.blade.php
@@ -1,41 +1,6 @@
 <header class="init">
     <div class="init_slider">
-
-        <div id="carousel_1" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carousel_1" data-slide-to="0" class="active"></li>
-              <li data-target="#carousel_1" data-slide-to="1"></li>
-              <li data-target="#carousel_1" data-slide-to="2"></li>
-            </ol>
-
-            <div class="carousel-inner" role="listbox">
-
-                <div class="item active">
-                    <img src="imagenes/slider/sl_home/bg1.jpeg" alt="img1">
-                    <div class="carousel-caption">
-                      <h3>lorem</h3>
-                      <p></p>
-                    </div>
-                </div>
-
-                <div class="item ">
-                    <img src="imagenes/slider/sl_home/bg2.jpeg" alt="img1">
-                    <div class="carousel-caption">
-                      <h3>lorem</h3>
-                      <p></p>
-                    </div>
-                </div>
-
-                <div class="item ">
-                    <img src="imagenes/slider/sl_home/bg3.jpeg" alt="img1">
-                    <div class="carousel-caption">
-                      <h3>lorem</h3>
-                      <p></p>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+         
 
     </div>
     @include('secciones.templates.nav')
