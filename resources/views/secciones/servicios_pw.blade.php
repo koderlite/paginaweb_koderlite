ab41610b0fcced796f3b419c4729cef17c6ff811
ariellugo92koder <ariellugo92koderlite@gmail.com>
2017-04-08 11:50:47 -0600

    modificando el responsive

--- resources/views/secciones/servicios_pw.blade.php
+++ resources/views/secciones/servicios_pw.blade.php
@@ -1,5 +1,5 @@
-<div class="row" id="serv-pg-prin">
-    <div class="col-sm-12 col-md-5">
+<div class="row-fluid" id="serv-pg-prin">
+    <div class="col-sm-12 col-md-4">
         <div id="fc-slideshow" class="fc-slideshow">
             <ul class="fc-slides">
                 <li><img src="{{ asset('imagenes/servicios/1.jpg') }}" /><h3>Hot</h3></li>
@@ -9,7 +9,7 @@
         </div>
     </div>
 
-    <div class="col-sm-12 col-md-7" id="serv-pag-derecha">
+    <div class="col-sm-12 col-md-8" id="serv-pag-derecha">
         <h3>PAGINAS WEB</h3>
         <p class="font-raleway">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur ex, deleniti at quaerat provident voluptates saepe quod hic temporibus repellat iste molestiae itaque expedita recusandae mollitia laborum ducimus porro exercitationem.</p>
 
