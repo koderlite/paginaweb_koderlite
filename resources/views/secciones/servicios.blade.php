d7776ef43b3461fae7350b6b392fdaccbc13824a
ariellugo92koder <ariellugo92koderlite@gmail.com>
2017-04-08 13:57:43 -0600

    modificando el servicio de disenio web

--- resources/views/secciones/servicios.blade.php
+++ resources/views/secciones/servicios.blade.php
@@ -1,82 +1,78 @@
-<div>
+<section class="main" id="serv-pnl-prin">
 
-    <section class="main" id="serv-pnl-prin">
+	<div class="container" id="serv-pnl-title">
+		<h3 class="ocultar animacion-li-title font-josefina negrita tam-2-medio-em">CONOCE NUESTROS SERVICIOS</h3>
+	</div>
 
-		<div class="container" id="serv-pnl-title">
-			<h3 class="ocultar animacion-li-title font-josefina negrita tam-2-medio-em">CONOCE NUESTROS SERVICIOS</h3>
-		</div>
-			
-				<ul class="ch-grid">
-					<li class="ocultar animacion-li-uno" id="serv-hover-item1">
-						<a href="#">
-							<div class="ch-item ch-img-1">				
-								<div class="ch-info-wrap">
-									<div class="ch-info serv-color-item1">
-										<div class="ch-info-front ch-img-1"></div>
-										<div class="ch-info-back serv-color-item1">
-											<h3>PAGINAS WEB</h3>
-											<p class="font-josefina">Llega a mas clientes con tu paginas hechas a la medida</p>
-										</div>	
-									</div>
-								</div>
+	<ul class="ch-grid">
+		<li class="ocultar animacion-li-uno" id="serv-hover-item1">
+			<a href="#">
+				<div class="ch-item ch-img-1">
+					<div class="ch-info-wrap">
+						<div class="ch-info serv-color-item1">
+							<div class="ch-info-front ch-img-1"></div>
+							<div class="ch-info-back serv-color-item1">
+								<h3>PAGINAS WEB</h3>
+								<p class="font-josefina">Llega a mas clientes con tu paginas hechas a la medida</p>
 							</div>
-						</a>
-						<br>
-						<p id="serv-caption-item1" class="font-raleway tam-1-medio-em ser-nombres">PAGINAS WEB</p>
-					</li>
-					<li class="ocultar animacion-li-dos">
-						<a href="#">
-							<div class="ch-item ch-img-2">
-								<div class="ch-info-wrap">
-									<div class="ch-info serv-color-item2">
-										<div class="ch-info-front ch-img-2"></div>
-										<div class="ch-info-back serv-color-item2">
-											<h3>WEB APPS</h3>
-											<p class="font-josefina">Desarrollamos apps web a la medida de tus necesidades</p>
-										</div>
-									</div>
-								</div>
+						</div>
+					</div>
+				</div>
+			</a>
+			<br>
+			<p id="serv-caption-item1" class="font-raleway tam-1-medio-em ser-nombres">PAGINAS WEB</p>
+		</li>
+		<li class="ocultar animacion-li-dos">
+			<a href="#">
+				<div class="ch-item ch-img-2">
+					<div class="ch-info-wrap">
+						<div class="ch-info serv-color-item2">
+							<div class="ch-info-front ch-img-2"></div>
+							<div class="ch-info-back serv-color-item2">
+								<h3>WEB APPS</h3>
+								<p class="font-josefina">Desarrollamos apps web a la medida de tus necesidades</p>
 							</div>
-						</a>
-						<br>
-						<p class="font-raleway tam-1-medio-em ser-nombres">DESARROLLO WEB</p>
-					</li>
-					<li class="ocultar animacion-li-tres">
-						<a href="#">
-							<div class="ch-item ch-img-3">
-								<div class="ch-info-wrap">
-									<div class="ch-info serv-color-item3">
-										<div class="ch-info-front ch-img-3"></div>
-										<div class="ch-info-back serv-color-item3">
-											<h3>E - COMMERCE</h3>
-											<p class="font-josefina">¡Vende tu producto o servicio a través de internet!</p>
-										</div>
-									</div>
-								</div>
+						</div>
+					</div>
+				</div>
+			</a>
+			<br>
+			<p class="font-raleway tam-1-medio-em ser-nombres">DESARROLLO WEB</p>
+		</li>
+		<li class="ocultar animacion-li-tres">
+			<a href="#">
+				<div class="ch-item ch-img-3">
+					<div class="ch-info-wrap">
+						<div class="ch-info serv-color-item3">
+							<div class="ch-info-front ch-img-3"></div>
+							<div class="ch-info-back serv-color-item3">
+								<h3>E - COMMERCE</h3>
+								<p class="font-josefina">¡Vende tu producto o servicio a través de internet!</p>
 							</div>
-						</a>
-						<br>
-						<p class="font-raleway tam-1-medio-em ser-nombres">E - COMMERCE</p>
-					</li>
-					<li class="ocultar animacion-li-cuatro">
-						<a href="#">
-							<div class="ch-item ch-img-4">
-								<div class="ch-info-wrap">
-									<div class="ch-info serv-color-item4">
-										<div class="ch-info-front ch-img-4"></div>
-										<div class="ch-info-back serv-color-item4">
-											<h3>APPS MOVILES</h3>
-											<p class="font-josefina">¡Todo fácil y rápido desde tus dispositivos móviles!</p>
-										</div>
-									</div>
-								</div>
+						</div>
+					</div>
+				</div>
+			</a>
+			<br>
+			<p class="font-raleway tam-1-medio-em ser-nombres">E - COMMERCE</p>
+		</li>
+		<li class="ocultar animacion-li-cuatro">
+			<a href="#">
+				<div class="ch-item ch-img-4">
+					<div class="ch-info-wrap">
+						<div class="ch-info serv-color-item4">
+							<div class="ch-info-front ch-img-4"></div>
+							<div class="ch-info-back serv-color-item4">
+								<h3>APPS MOVILES</h3>
+								<p class="font-josefina">¡Todo fácil y rápido desde tus dispositivos móviles!</p>
 							</div>
-						</a>
-						<br>
-						<p class="font-raleway tam-1-medio-em ser-nombres">APPS MOVILES</p>
-					</li>
-				</ul>
-				
-			</section>
+						</div>
+					</div>
+				</div>
+			</a>
+			<br>
+			<p class="font-raleway tam-1-medio-em ser-nombres">APPS MOVILES</p>
+		</li>
+	</ul>
 
-</div>
+</section>
--- resources/views/secciones/servicios_pw.blade.php
+++ resources/views/secciones/servicios_pw.blade.php
@@ -1,19 +1,19 @@
-<div class="row-fluid" id="serv-pg-prin">
-    <div class="col-sm-12 col-md-4">
+<div class="col-sm-12 col-md-12" id="serv-pg-prin">
+    <div class="col-sm-12 col-md-4" id="serv-pw-imgs">
         <div id="fc-slideshow" class="fc-slideshow">
             <ul class="fc-slides">
-                <li><img src="{{ asset('imagenes/servicios/1.jpg') }}" /><h3>Hot</h3></li>
-                <li><img src="{{ asset('imagenes/servicios/2.jpg') }}" /><h3>Cold</h3></li>
-                <li><img src="{{ asset('imagenes/servicios/3.jpg') }}" /><h3>Light</h3></li>
+                <li><img width="300" height="300" src="{{ asset('imagenes/servicios/servicio-pagina-web-1.png') }}" /><h3>Creativas</h3></li>
+                <li><img width="300" src="{{ asset('imagenes/servicios/2.jpg') }}" /><h3>Unicas</h3></li>
+                <li><img width="300" src="{{ asset('imagenes/servicios/3.jpg') }}" /><h3>Adaptables</h3></li>
             </ul>
         </div>
     </div>
 
     <div class="col-sm-12 col-md-8" id="serv-pag-derecha">
-        <h3>PAGINAS WEB</h3>
-        <p class="font-raleway">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur ex, deleniti at quaerat provident voluptates saepe quod hic temporibus repellat iste molestiae itaque expedita recusandae mollitia laborum ducimus porro exercitationem.</p>
+        <h3>DISEÑO WEB</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur ex, deleniti at quaerat provident voluptates saepe quod hic temporibus repellat iste molestiae itaque expedita recusandae mollitia laborum ducimus porro exercitationem.</p>
 
-        <div class="text-right">
+        <div class="text-right" id="serv-pw-btn">
             <button class="btn btn-primary">VER MAS</button>
         </div>
     </div>
