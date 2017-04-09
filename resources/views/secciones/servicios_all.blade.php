5d2b964f2c903e25c853896b81bb7ed0b3a16286
ariellugo92koder <ariellugo92koderlite@gmail.com>
2017-04-09 01:50:05 -0600

    agregando el contenido en texto de los servicios especificos

--- resources/views/secciones/servicios_all.blade.php
+++ resources/views/secciones/servicios_all.blade.php
@@ -1,27 +1,27 @@
 {{-- servicio de disenio web --}}
-<div class="col-sm-12 col-md-12 serv-pg-prin">
+<div class="col-sm-12 col-md-12 serv-pg-prin" id="serv-1-prin">
     <div class="col-sm-12 col-md-4 serv-pw-imgs">
-        <div id="fc-slideshow" class="fc-slideshow">
+        <div id="fc-slideshow" class="fc-slideshow color-box-shadow">
             <ul class="fc-slides">
                 <li>
                     <img width="300" height="300" src="{{ asset('imagenes/servicios/servicio-pagina-web-1.png') }}"/>
-                    <h3>Creativas</h3>
+                    <h3 class="color-celeste">Creativas</h3>
                 </li>
                 <li>
                     <img width="300" height="300" src="{{ asset('imagenes/servicios/2.jpg') }}" />
-                    <h3>Unicas</h3>
+                    <h3 class="color-celeste">Unicas</h3>
                 </li>
                 <li>
                     <img width="300" height="300" src="{{ asset('imagenes/servicios/3.jpg') }}" />
-                    <h3>Adaptables</h3>
+                    <h3 class="color-celeste">Adaptables</h3>
                 </li>
             </ul>
         </div>
     </div>
 
-    <div class="col-sm-12 col-md-8 serv-pag-derecha">
+    <div class="col-sm-12 col-md-8 serv-pag-derecha" id="serv-1">
         <h3>DISEÑO WEB</h3>
-        <p>Lorem dolor sit amet, consectetur adipisicing elit. Consectetur ex, deleniti at quaerat provident voluptates saepe quod hic temporibus repellat iste molestiae itaque expedita recusandae mollitia laborum ducimus porro exercitationem.</p>
+        <p>Creamos y dise&ntilde;amos tu pagina web de acuerdo con la imagen corporativa y los objetivos de tu empresa. Te garantizamos un dise&ntilde;o web fácilmente entendible, agradable y que comunique claramente el mensaje que deseas que el visitante reciba y recuerde.</p>
 
         <div class="text-right serv-pw-btn">
             <button class="btn btn-primary">VER MAS</button>
@@ -31,12 +31,12 @@
 {{-- /design_w --}}
 
 {{-- servicio de desarrollo web --}}
-<div class="col-sm-12 col-md-12 serv-pg-prin">
-    <div class="col-sm-12 col-md-8 serv-pag-derecha">
-        <h3>DISEÑO WEB</h3>
-        <p>Lorem dolor sit amet, consectetur adipisicing elit. Consectetur ex, deleniti at quaerat provident voluptates saepe quod hic temporibus repellat iste molestiae itaque expedita recusandae mollitia laborum ducimus porro exercitationem.</p>
+<div class="col-sm-12 col-md-12 serv-pg-prin" id="serv-2-prin" style="background: #C90000;">
+    <div class="col-sm-12 col-md-8 serv-pag-derecha" id="serv-2">
+        <h3 class="color-letra-blanco">DESAROLLO WEB</h3>
+        <p class="color-letra-blanco">Administra, controla y organiza tu negocio o empresa con un sistema web a la medida de tus necesidades. Hacemos de tu aplicacion un sistema totalmente escalable, robusto, <strong>seguro</strong> y confiable.</p>
 
-        <div class="text-right serv-pw-btn">
+        <div class="text-left serv-pw-btn">
             <button class="btn btn-primary">VER MAS</button>
         </div>
     </div>
@@ -63,7 +63,7 @@
 {{-- dev_web --}}
 
 {{-- servicio de desarrollo e-commerce --}}
-<div class="col-sm-12 col-md-12 serv-pg-prin">
+<div class="col-sm-12 col-md-12 serv-pg-prin" id="serv-3-prin" style="background: #e67e22;">
     <div class="col-sm-12 col-md-4 serv-pw-imgs">
         <div id="fc-slideshow3" class="fc-slideshow">
             <ul class="fc-slides">
@@ -83,9 +83,9 @@
         </div>
     </div>
 
-    <div class="col-sm-12 col-md-8 serv-pag-derecha">
-        <h3>DISEÑO WEB</h3>
-        <p>Lorem dolor sit amet, consectetur adipisicing elit. Consectetur ex, deleniti at quaerat provident voluptates saepe quod hic temporibus repellat iste molestiae itaque expedita recusandae mollitia laborum ducimus porro exercitationem.</p>
+    <div class="col-sm-12 col-md-8 serv-pag-derecha" id="serv-3">
+        <h3 class="color-letra-blanco">E - COMMERCE</h3>
+        <p class="color-letra-blanco">Supera los limites geografico de tu tienda y ten cobertura nacional, tendras accesibilidad a nuevos clientes y estos tendran la facilidad de adquirir tus servicios desde la comodidad de su casa. Te garantizamos una tienda atractiva, facil de encontrar y con la seguridad que el equipo de <strong>KODERLITE</strong> te brinda.</p>
 
         <div class="text-right serv-pw-btn">
             <button class="btn btn-primary">VER MAS</button>
@@ -95,12 +95,12 @@
 {{-- /e-commerce --}}
 
 {{-- servicio de desarrollo movil --}}
-<div class="col-sm-12 col-md-12 serv-pg-prin">
-    <div class="col-sm-12 col-md-8 serv-pag-derecha">
-        <h3>DISEÑO WEB</h3>
-        <p>Lorem dolor sit amet, consectetur adipisicing elit. Consectetur ex, deleniti at quaerat provident voluptates saepe quod hic temporibus repellat iste molestiae itaque expedita recusandae mollitia laborum ducimus porro exercitationem.</p>
+<div class="col-sm-12 col-md-12 serv-pg-prin" id="serv-4-prin" style="background: #029a87;">
+    <div class="col-sm-12 col-md-8 serv-pag-derecha" id="serv-4">
+        <h3 class="color-letra-blanco">DESAROLLO MOVIL</h3>
+        <p class="color-letra-blanco">Organiza tu inventario, clientes, vendedores o personal con una aplicacion movil, esto te ayudara a ahorrarte costos de papeleria registrando las actividades de tu dia a dia en tu app desarollada a tu medida o dependiendo de tu giro de negocio.</p>
 
-        <div class="text-right serv-pw-btn">
+        <div class="text-left serv-pw-btn">
             <button class="btn btn-primary">VER MAS</button>
         </div>
     </div>
@@ -110,15 +110,15 @@
             <ul class="fc-slides">
                 <li>
                     <img width="300" height="300" src="{{ asset('imagenes/servicios/servicio-pagina-web-1.png') }}"/>
-                    <h3 class="color-celeste">Creativas</h3>
+                    <h3>Creativas</h3>
                 </li>
                 <li>
                     <img width="300" height="300" src="{{ asset('imagenes/servicios/2.jpg') }}" />
-                    <h3 class="color-celeste">Unicas</h3>
+                    <h3>Unicas</h3>
                 </li>
                 <li>
                     <img width="300" height="300" src="{{ asset('imagenes/servicios/3.jpg') }}" />
-                    <h3 class="color-celeste">Adaptables</h3>
+                    <h3>Adaptables</h3>
                 </li>
             </ul>
         </div>
