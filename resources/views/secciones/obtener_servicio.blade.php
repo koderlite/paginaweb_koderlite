8585d7ff4d755b6d89d4875e41de5137cb8d8b6d
eocongarcia <eocongarcia@gmail.com>
2017-04-09 00:27:35 -0600

    inicio de obtener servicio

---
+++ resources/views/secciones/obtener_servicio.blade.php
@@ -0,0 +1,14 @@
+<div class="obt_serv_content container-fluid">
+    <div class="row">
+      <div class="col-md-6">
+          
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+    <div class="obt_serv_bg">
+
+    </div>
+
+</div>
