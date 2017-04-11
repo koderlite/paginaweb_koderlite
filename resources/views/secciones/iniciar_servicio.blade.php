e3b61c25ebef4579a0dd0b74a79e301a2fdb558a
eocongarcia <eocongarcia@gmail.com>
2017-04-11 01:27:16 -0600

    1 parte del left terminado

--- resources/views/secciones/iniciar_servicio.blade.php
+++ resources/views/secciones/iniciar_servicio.blade.php
@@ -1,19 +1,50 @@
 <div id="empezar" class="init_serv_content container-fluid">
     <div class="row">
-      <div class="col-md-6">
-          <h2>Inicia tu proyecto</h2>
-
-          <p> 
-
-          </p>
-
+      <div class="col-md-6 init_serv_left">
+          
+          <div class="left_static">
+              <h3>Inicia tu proyecto con nosotros</h3>
+              <h4>en 3 sencillos pasos</h4>
+              <ul class="static_steps">
+                <li class="steps_dots1"><div class="active">
+                  
+                </div></li>
+                <li class="steps_dots2"><div>
+                  
+                </div></li>
+                <li class="steps_dots3"><div>
+                  
+                </div></li>
+              </ul>
+          </div>
+          <div class="left_dinamic">
+            <div class="left_slider">
+              <div class="slide_1">
+                  <p><i class="material-icons">touch_app</i> Selecciona un servicio</p>
+                  <div class="sl_img">
+                    
+                  </div>
+              </div>
+              <div class="slide_2">
+                  <p><i class="material-icons">keyboard</i>Ingresa tus datos y los de tu negocio</p>
+                  <div class="sl_img">
+                    
+                  </div>
+              </div>
+              <div class="slide_3">
+                  <p><i class="material-icons">send</i>Envia la informacion</p>
+                  <div class="sl_img">
+                    
+                  </div>
+              </div>
+            </div>
+          </div>
       </div>
-      <div class="col-md-6">
-
+      <div class="col-md-6 init_serv_right">
+          
       </div>
     </div>
     <div class="obt_serv_bg">
-  
     </div>
 
 </div>
