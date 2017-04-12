882559b3693916be18fe5530912f5242eb2de011
eocongarcia <eocongarcia@gmail.com>
2017-04-12 01:13:38 -0600

    iniciando diseno de la seccion empezar

--- resources/views/secciones/iniciar_servicio.blade.php
+++ resources/views/secciones/iniciar_servicio.blade.php
@@ -3,31 +3,38 @@
       <div class="col-md-6 init_serv_left">
           
           <div class="left_static">
-              <h3>Inicia tu proyecto con nosotros</h3>
+              <div class="init_serv_title">
+                
+                <h3>Inicia tu proyecto con nosotros</h3>
+
+              </div>
               
           </div>
           <div class="left_dinamic">
             <div class="left_slider">
               <div class="slide_0 active">
-                  <p><i class="material-icons">format_list_numbered</i> En 3 sencillos pasos</p>
+                   <p>En 3 sencillos pasos</p>
                   <div class="sl_img">
                     
                   </div>
               </div>
               <div class="slide_1">
-                  <p><i class="material-icons">touch_app</i> Selecciona un servicio</p>
+                  <i class="material-icons">touch_app</i> 
+                  <p>Selecciona un servicio</p>
                   <div class="sl_img">
                     
                   </div>
               </div>
               <div class="slide_2">
-                  <p><i class="material-icons">keyboard</i>Ingresa tus datos y los de tu negocio</p>
+                  <i class="material-icons">keyboard</i>
+                  <p>Ingresa tus datos y los de tu negocio</p>
                   <div class="sl_img">
                     
                   </div>
               </div>
               <div class="slide_3">
-                  <p><i class="material-icons">send</i>Envia la informacion</p>
+                  <i class="material-icons">send</i>
+                  <p>Envia la informacion</p>
                   <div class="sl_img">
                     
                   </div>
